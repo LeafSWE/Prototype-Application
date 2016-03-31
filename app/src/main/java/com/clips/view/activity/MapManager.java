55175e3a3399e2d1034c1d2567156f6c319ea23a
OctaTux <octatux@gmail.com>
2016-03-31 22:35:39 +0200

    Aggiunto mapManager

---
+++ app/src/main/java/com/clips/view/activity/MapManager.java
@@ -0,0 +1,13 @@
+package com.clips.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_manager);
+    }
+}
